22c231b52b5457735e4a01334256c29d629d75d3
Gurvir Singh <charizardzx9@gmail.com>
2021-10-11 12:02:31 -0400

    cleaned up code significantly will test this comming wednesday

--- Robot.java
+++ Robot.java
@@ -6,7 +6,6 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
-//package org.usfirst.frc.team1403.robot;
 
 import java.lang.ModuleLayer.Controller;
 import java.sql.Driver;
@@ -32,35 +31,48 @@ import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.MjpegServer;
 import edu.wpi.cscore.UsbCamera;
-
 //import edu.wpi.first.wpilibj.Encoder;
 //import cougarecho.record.Recorder;
 //import sun.jvm.hotspot.ci.ciMethod;
 //import sun.security.provider.CtrDrbg;
 
 
-
-
-
-
-/**
+/*
+ * Your best friend: https://first.wpi.edu/FRC/roborio/release/docs/java/
+ * the link above is the documentation for the classes and library we use
+ *
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
  * directory.
- */
-/**
+ *
  * TimedRobot: It provides control of the robot program through a collection of init() and periodic() methods,
  * which are called by WPILib during specific robot states (e.g. autonomous or teleoperated).
  * By default, periodic methods are called every 20ms; this can be changed by overriding the getPeriod() method.
  * 
+ * Any method with @Overide above it is a function in the class TimedRobot which is what our code inherits from. We override them with
+ * the functionality we need from out robot and shouldnt be deleted. Deleting them wont do anything and you can reimplment them later
+ * but I recommend not removing them for simplicity. The other methods are ones we defined completly for ourselves and I prefer to put
+ * them near the top of the code. These are used to simplify code and make code snippets reusable. An example being the Launcher()
+ * method that should set the launcher to the speed you pass in and make sures they spin in oppisite directions. That way we just need
+ * to call Luancher when we need it instead of rewriting it everytime we need to set the speed of the launcher. 
  * 
- * TO DO:
- * 1. Test Motors (Uncomment if Nec.)
- * 2. Lifter Arms
- * 3. Fix Lighting
+ * Here is the explanation for all the methods we overide from the TimedRobot class:
+ * Note: If you want more info on any of these or see the other ones we havent implemented you can find it in the documentation below: 
+ * https://first.wpi.edu/FRC/roborio/release/docs/java/
  * 
+ * robotInit() is the function run when the robot starts up. Use this for code you need to run when the robot is enabled
+ * 
+ * teleopInit() is the function run when the robot starts into tele-operated mode (when you control the robot remotly).
+ * 
+ * teleopPeriodic() is the function run consantly when in tele-operated mode. A very simplified explanation is while the robot is in 
+ * tele-operated mode it will do the instructions in this function every 20ms. So this is where stuff like your drive and shooting go
+ * 
+ * autonomousInit() is the function run when the robot starts into autonomous mode (when the robot moves on its own).
+ * 
+ * autonomousPeriodic() is the function run consantly when in autonomous mode. A very simplified explanation is while the robot is in 
+ * autonomous mode it will do the instructions in this function every 20ms. So this is where stuff like drive and shooting go
  */
 public class Robot extends TimedRobot {
 
@@ -80,98 +92,67 @@ public class Robot extends TimedRobot {
   private final MjpegServer       jpegServer1   = new MjpegServer("serve_USB Camera", 25); //mJpegServer
   private final CvSink            cvSink        = new CvSink("opencv_USB Camera 0");    //CvSink
 
-  //sample
-
-    
-  public static boolean toggleOn;
-  public static boolean Truetoggle;
   public static boolean controlsReversed;
   public static boolean driveSwitch;
-  public static boolean togglePressed2;
 
-    
-
-  /*
-  private int numpaths;
-	private String path;
-  public static Recorder recorder;
-	public static boolean record;
-	public static boolean store;*(/)
-
-
-
-  
-  //private final AnalogInput       m_distance    = new AnalogInput(0);                   //Distance Sensor
-    /*
-    @Override
-    public void enable() {
-      // TODO Auto-generated method stub
-      
-    }
-  
-    @Override
-    public void disable() {
-      // TODO Auto-generated method stub
-      
-    }
+  public void Launcher(double speed)
+  {
+    m_launcher1.setSpeed(speed);             //Bottom
+    m_launcher2.setSpeed(-(speed - 0.1));    //Top
   }
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  
   @Override
   public void robotInit() {
-  
-    /*
-  jpegServer1.setSource(camera);
-  cvSink.setSource(camera);
-  */
-  //m_intake.setSpeed(-0.7);
-  //m_launcher2.setSpeed(-0.7);
-
-  toggleOn = true;
-
-  Truetoggle = true;
-  togglePressed2 = true;
-
-
-  //False = Normal Controls, True = Reversed Controls
-  controlsReversed = false;
-  
-  
-  //Keep false
-  driveSwitch = false;
-  togglePressed2 = false;
-
-  /*recorder = new Recorder(10000);
-  boolean toggleOn = false;
-  boolean togglePressed = false;
-
-  numpaths = 0;
-	init();
-  recorder.setCurrentWritefile(1);
-	recorder.setCurrentReadfile(0);
-	Recorder.initWriter();
-  Recorder.initReader();*/
-  
+    controlsReversed = false;
+    driveSwitch = false;
   }
-  /**
-   * This function is run once each time the robot enters autonomous mode.
-   */
+
+  @Override
+  public void teleopInit() {
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+
+    if (m_stick.getTriggerReleased()) {
+      m_intake.setSpeed(0.2);
+    }
+
+    if (m_stick.getRawButtonReleased(2))
+    {
+      controlsReversed = !controlsReversed;
+    }
+
+    if (m_stick.getRawButtonReleased(3)) {
+      Launcher(0.2);
+    }
+
+    if (m_stick.getRawButtonReleased(12)) 
+    {
+      m_launcher1.stopMotor();
+      m_launcher2.stopMotor();
+      m_intake.stopMotor();
+    }
+
+    if (controlsReversed)
+    {
+      m_robotDrive.arcadeDrive((m_stick.getY()),-m_stick.getX(), true);
+    }
+    else
+    {
+      m_robotDrive.arcadeDrive(-(m_stick.getY()), (m_stick.getX()), true);
+    }
+  }
+  
   @Override
   public void autonomousInit() {
     m_timer.reset();
     m_timer.start();
-
    // m_encoder.reset(); //resets encoder to 0
    // m_encoder.setDistancePerPulse(1.0/360.0);
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
   @Override
   public void autonomousPeriodic() {
 
@@ -217,335 +198,4 @@ public class Robot extends TimedRobot {
     //m_robotDrive.arcadeDrive(0.3,0.0);
   }
 
-  /**
-   * This function is called once each time the robot enters teleoperated mode.
-   */
-  @Override
-  public void teleopInit() {
-  }
-
-  /**
-   * This function is called periodically during teleoperated mode.
-   */
-  @Override
-  public void teleopPeriodic() {
-    
-
-   
-
-    
-
-  /*if (m_stick.getY() < 0.2 && m_stick.getY() > -0.2)
-    {
-      if (m_stick.getRawButton(1))
-      {
-        updateToggle();
-      }
-      if (togglePressed == true)
-      {
-        m_robotDrive.stopMotor();
-        change = -1;
-      }
-      if (togglePressed == false)
-      {
-        m_robotDrive.stopMotor();
-        change = 1;
-      }
-    }
-
-    //Drive(-(m_stick.getY()), m_stick.getX());
-    m_robotDrive.arcadeDrive(change * -(m_stick.getY()),change * m_stick.getX());*/
-
-    if (m_stick.getTriggerReleased()) {
-      m_intake.setSpeed(0.2);
-    }
-    if (m_stick.getRawButtonReleased(2))
-    {
-      //ReverseControls();
-      controlsReversed = !controlsReversed;
-      //System.out.println(m_intake.getRaw());
-      
-      /*
-      try {
-        Thread.sleep(500);
-      } catch (InterruptedException e) {
-        //TODO: handle exception
-      }*/
-    }
-    if (m_stick.getRawButtonReleased(3)) {
-      Launcher(0.2);
-    }
-    if (m_stick.getRawButtonReleased(5)) //if button A is pressed
-    {
-      System.out.println("intake button pressed");
-        //Turn on intake motor
-      //Intake();
-    }
-    if (m_stick.getRawButtonReleased(12)) //if button B is pressed
-    {
-      m_launcher1.stopMotor();
-      m_launcher2.stopMotor();
-      m_intake.stopMotor();
-      //System.out.println("outtake button pressed");
-      //Outtake(); //Turn on outtake motor
-    }
-    if (controlsReversed)
-    {
-      //System.out.println("Button has been toggled");
-      if (!driveSwitch) //Joystick
-      {
-          m_robotDrive.arcadeDrive((m_stick.getY()),-m_stick.getX(), true);
-      }
-      else 
-      {
-          //m_robotDrive.tankDrive(m_controller.getY(Hand.kLeft),m_controller.getY(Hand.kRight));
-          m_robotDrive.tankDrive(m_stick.getY(),m_stick_left.getY());
-      }
-    }
-    else
-    {
-      //System.out.println("Button has not been toggled");
-      if (!driveSwitch) //Joystick
-      {
-          m_robotDrive.arcadeDrive(-(m_stick.getY()), (m_stick.getX()), true);
-      }
-      else
-      {
-        //m_robotDrive.tankDrive(-m_controller.getY(Hand.kLeft),-m_controller.getY(Hand.kRight))
-        m_robotDrive.tankDrive(-(m_stick_left.getY()),-(m_stick.getY()));
-      }
-    }
-
-
-    //This controls the intake and outtake motor (tank drive)
-    //if (m_stick.getRawButton(6)) //If the Joystick's Trigger is pressed
-    /*
-    if (m_controller.getY() > -0.69)
-    {
-      //Intake();
-      //System.out.println("intake button pressed");
-      m_intake.setSpeed(0.70); //speed is set to 0.70
-    }
-    
-    else if(m_controller.getY() < -0.69) //If the controller's joystick y axis is less than 0.69
-    {
-      m_intake.setSpeed(-0.70); //speed is set to -0.70
-    }
-    else if(m_controller.getY() > 0.1 || m_controller.getY() < -0.1) //if the controlller's joystick y axis is between 0.1 and -0.1
-    {
-      m_intake.setSpeed(- (m_controller.getY())); //User controlled speed
-    }
-    else if (m_stick.getRawButtonReleased(6)) //if button A is pressed
-    {
-      System.out.println("intake button pressed");
-      //m_intake.setSpeed(1.00);  //Turn on intake motor
-      Intake();
-    }
-    else if (m_stick.getRawButtonReleased(3)) //if button B is pressed
-    {
-      m_intake.stopMotor();
-      System.out.println("outtake button pressed");
-      //Outtake(); //Turn on outtake motor
-    }
-    else
-    {
-      m_intake.stopMotor(); //Stop intake motor
-    }
-    */
-    
-     
-
-    //Launch
-
-    /*
-    if (m_controller.getRawButton(2)) 
-    {
-    
-      m_launcher1.setSpeed(0.6);  //Bottom
-      m_launcher2.setSpeed(-0.5); //Top
-    }
-    else if (m_controller.getRawButton(4))
-    {
-      //Launcher(-0.3);
-    }
-    else 
-    {
-      m_launcher1.stopMotor();
-      m_launcher2.stopMotor();
-    }*/
-
-    if(m_controller.getRawButtonPressed(7))
-    {
-     // SwitchDrive();
-      try {
-        Thread.sleep(100);
-      } catch (InterruptedException e) {
-        //TODO: handle exception
-      }
-    }
-
-
-    
-    if(m_stick.getRawButtonPressed(7))
-    {
-     // SwitchDrive();
-      try {
-        Thread.sleep(100);
-      } catch (InterruptedException e) {
-        //TODO: handle exception
-      }
-    }
-
-
-  //Lighting
-                                  // sensor code. Might work? Probably not.
-    /*double gapLength = 60.96;
-
-    double currentdistance = (m_distance.getValue() / 9.77);
-
-    if (currentdistance < gapLength)
-    {
-      Drive(0.2,0.0); 
-    }
-    else Stop();*/
-
-
-    /*if (m_stick.getDirectionDegrees() <= 20  || (m_stick.getDirectionDegrees() >=  70 && m_stick.getDirectionDegrees() <= 110))
-    {
-      m_robotDrive.arcadeDrive(-(m_stick.getY()), m_stick.getX());
-    }
-    else m_robotDrive.stopMotor();*/
-
-    //REVISE + 
-    
-    //INTAKE OUTTAKE CONTROLS
-    
-    //Intake
-    if(m_stick.getRawButtonPressed(3)) 
-    {
-      //m_light.set(0.61);
-     // m_intake.setSpeed(-0.4);
-    }
-    //Outtake
-    else if(m_stick.getRawButton(4)) 
-    {
-      m_light.set(0.69);
-      //m_intake.setSpeed(0.4);
-    }
-    
-    if (m_stick.getRawButtonPressed(6))
-    {
-      //m_intake.stopMotor();
-    }
-
-
-    /*
-    if (m_controller.getRawButton(1))
-    {
-      m_light.set(0.77); //Green Light
-    }
-    else if (m_controller.getRawButton(2))
-    {
-      m_light.set(0.61); //Red Light
-    }
-    else if (m_controller.getRawButton(4))
-    {
-      m_light.set(0.69); //Yellow Light
-    }
-    else if (m_controller.getRawButton(3))
-    {
-      m_light.set(0.87); //Blue Light
-    }
-    else 
-    {
-      m_light.stopMotor(); //No Light
-    }
-
-    
-    m_robotDrive.arcadeDrive(-(m_stick.getY()), m_stick.getX());
-   */ 
-    //m_robotDrive.tankDrive(-m_controller.getY(Hand.kLeft) / 2, -m_controller.getY(Hand.kRight) / 2 );
-    
-
-    //m_left.setSpeed(m_stick.getY()/8);
-   // m_right.setSpeed(-(m_stick.getY()/8)); //Testing to see if running both of them also makes it pop.
-    /*if(m_stick.getRawButton(4)) 
-    {
-      m_light.set(0.61);
-      m_left.setSpeed(-0.4);
-    }
-    
-    
-    m_intake.setSpeed(-(m_stick.getY() / 2));
-*/
-  }
-  
-  
-  
-  //Reverses all controls by toggling togglePressed
-  public void ReverseControls()
-  {
-    controlsReversed = !controlsReversed;
-  }
-
-  /*
-  public void SwitchDrive()
-  {
-    if (!togglePressed2)
-    {
-       driveSwitch = true;
-       togglePressed2 = true;
-    }
-    else
-    {
-      driveSwitch = false;
-      togglePressed2 = false;
-    }
-  }
-
-  public void Drive(double Mspeed, double Mrotation)
-  {
-    if ((Mspeed <= 1 && Mspeed >= -1) && (Mrotation <= 1 && Mrotation >= -1))
-    {
-      m_robotDrive.arcadeDrive(Mspeed,Mrotation);
-    }
-  }
-  */
-  public void Intake()
-  {
-    //m_light.set(0.61);
-    
-    
-
-    
-  }
-  
-
-  public void Outtake()
-  {
-    //m_light.set(0.69);
-    //m_intake.setSpeed(-0.7);
-  }
-
-  public void Launcher(double speed)
-  {
-    m_launcher1.setSpeed(speed);             //Bottom
-    m_launcher2.setSpeed(-(speed - 0.1));    //Top
-  }
-
-  public void Stop()
-  {
-    m_robotDrive.stopMotor();
-  }
-
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-
-  }
 }
-
-
-//
